82f7f6e8b50eaadb88646a1f39c4c95b5f8973b2
David E Lares S <david.e.lares@gmail.com>
2019-03-05 13:38:52 -0600

    Custom validation, overriding Form.validate() method

--- app/forms.py
+++ app/forms.py
@@ -4,6 +4,11 @@ from wtforms import StringField, PasswordField, BooleanField
 from wtforms import validators
 from wtforms.fields.html5 import EmailField
 
+# custom validation
+def custom_validator(form, field): # mandatory params
+    if field.data == "david" or field.data == "davidlares":
+        raise validators.ValidationError('That username is not allowed')
+
 class LoginForm(Form):
     # attrs = inputs
     username = StringField('Username', [
@@ -16,7 +21,9 @@ class LoginForm(Form):
 class RegisterForm(Form):
     # attrs
     username = StringField('Username', [
-        validators.length(min=4,max=50)
+        validators.length(min=4,max=50),
+        # adding custom validator
+        custom_validator
     ])
 
     email = EmailField('Email', [
@@ -34,3 +41,21 @@ class RegisterForm(Form):
     accept = BooleanField('', [
         validators.DataRequired()
     ])
+
+    # validating from a method
+    def validate_username(self, username):
+        if User.get_by_username(username.data):
+            raise validators.ValidationError('Username already in use')
+
+    def validate_email(self, email):
+        if User.get_by_email(email.data):
+            raise validators.ValidationError('Email already in use')
+
+    # overriding the validate method
+    def validate(self):
+        if not Form.validate(self): # actually run the previous validators before
+            return False
+        if len(self.password) < 3:
+            self.password.errors.append('Password is too short')
+            return False
+        return True
